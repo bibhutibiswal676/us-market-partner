d02fd231b889e12f2ac593238b4b172838c0c924
Shyamsaitejamandibi <ee21b127@smail.iitm.ac.in>
2025-09-17 13:20:27 +0530

    change sitemap and robots

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,8 +3,8 @@ import type { MetadataRoute } from 'next';
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://www.example.com/',
-      lastModified: new Date(),
+      url: 'https://www.usmarketpartner.com/',
+      lastModified: new Date('2025-09-05T12:07:37+00:00'),
       changeFrequency: 'weekly',
       priority: 1,
     },
